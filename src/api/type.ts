cc50248db71843ffe2cd006b422ce553792971e7
InsectMk <makun_ing@126.com>
2025-03-28 11:39:11 +0800

    chore: 增加分页查询统一返回/请求对象格式

--- src/api/type.ts
+++ src/api/type.ts
@@ -2,7 +2,6 @@
  * 通用返回对象
  */
 export interface CommonResult<T> {
-
   /**
    * 错误码
    */
@@ -15,4 +14,32 @@ export interface CommonResult<T> {
    * 错误提示，用户可阅读
    */
   msg: string
+}
+
+/**
+ * 分页结果
+ */
+export interface PageResult<T> {
+  /**
+   * 数据
+   */
+  list: T[]
+  /**
+   * 总量
+   */
+  total: number
+}
+
+/**
+ * 分页参数
+ */
+export interface PageParam {
+  /**
+   * 页码 从1开始
+   */
+  pageNo: number
+  /**
+   * 每页条数
+   */
+  pageSize: number
 }
