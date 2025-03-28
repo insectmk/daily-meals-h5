0246a8972ecd4bc505b31804991392cfb7888484
InsectMk <makun_ing@126.com>
2025-03-28 15:48:54 +0800

    feat: 增加菜谱详情页面的跳转

---
+++ src/pages/recipe-cache/[id].vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+import { getRecipeInfo } from '@/api/recipe'
+import type { RecipeInfo } from '@/api/recipe/type'
+
+const route = useRoute()
+
+const id = route.params.id // 路由参数：菜谱ID
+const recipe = ref<RecipeInfo>()
+const loading = ref<boolean>(true) // 加载中
+/**
+ * 获取菜谱信息
+ */
+getRecipeInfo({ id }).then((res) => {
+  recipe.value = res.data
+  loading.value = false // 加载完毕
+})
+</script>
+
+<template>
+  <div v-if="loading">
+    <van-loading size="24px">
+      加载中...
+    </van-loading>
+  </div>
+  <div v-else>
+    <h1 class="text-6xl color-pink font-semibold">
+      {{ recipe.name }}
+    </h1>
+
+    <p class="mt-4 text-gray-700 dark:text-white">
+      {{ recipe.recipeDesc }}
+    </p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<route lang="json5">
+{
+  name: 'RecipeInfo',
+  meta: {
+    title: '菜谱信息',
+    i18n: 'recipe.title.info',
+  },
+}
+</route>
