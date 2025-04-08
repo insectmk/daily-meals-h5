7021f5ad997f24cd611710b6db05e4ece0b65263
InsectMk <makun_ing@126.com>
2025-04-08 14:41:28 +0800

    feat: 增加获取字段数据信息的接口

---
+++ src/api/system/type.ts
@@ -0,0 +1,18 @@
+export interface DictInfo {
+  /**
+   * 字典ID
+   */
+  id: number
+  /**
+   * 描述
+   */
+  label: string
+  /**
+   * 值
+   */
+  value: string
+  /**
+   * 字典类型
+   */
+  dictType: string
+}
