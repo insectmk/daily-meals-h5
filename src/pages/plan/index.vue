84e087c1b7d600c0b773f60e5977d5d956f4a765
InsectMk <makun_ing@126.com>
2025-04-16 16:44:11 +0800

    chore: 暂时去除计划下滑刷新的功能

--- src/pages/plan/index.vue
+++ src/pages/plan/index.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
 import { page as getDailyPlansPage } from '@/api/dailyplan'
-import { computed } from 'vue'
 import type { DailyPlanInfo } from '@/api/dailyplan/type'
 import type { CalendarDayItem } from 'vant'
 
@@ -18,29 +17,10 @@ const loading = ref(false) // 加载
 const finished = ref(false) // 结束
 // const { t } = useI18n() // 多语言支持
 
-// 计划最小时间
-const minDate = computed(() => {
-  if (dailyPlanList.value.length === 0)
-    return new Date()
-  return new Date(dailyPlanList.value.reduce((minPlanDate, dailyPlan) =>
-    dailyPlan.planDate < minPlanDate
-      ? dailyPlan.planDate
-      : minPlanDate, dailyPlanList.value[0]?.planDate ?? 0))
-})
-// 计划最大时间
-const maxDate = computed(() => {
-  if (dailyPlanList.value.length === 0)
-    return new Date()
-  return new Date(dailyPlanList.value.reduce((maxPlanDate, dailyPlan) =>
-    dailyPlan.planDate > maxPlanDate
-      ? dailyPlan.planDate
-      : maxPlanDate, dailyPlanList.value[0]?.planDate ?? 0))
-})
-
 /**
- * 加载
+ * 加载计划
  */
-function onLoad() {
+function loadPlan() {
   pageNo++ // 页面加1
   // 获取菜单
   getDailyPlansPage({
@@ -57,6 +37,11 @@ function onLoad() {
     }
   })
 }
+
+onMounted(() => {
+  // 加载计划
+  loadPlan()
+})
 
 /**
  * 自定义日期内容
@@ -116,26 +101,17 @@ onBeforeRouteLeave(() => {
 </script>
 
 <template>
-  <van-list
+  <van-calendar
     v-if="!loading"
-    v-model:loading="loading"
-    :finished="finished"
-    finished-text="已经到底啦 ~"
-    loading-text="加载中..."
-    @load="onLoad"
-  >
-    <van-calendar
-      :formatter="formatter"
-      :min-date="minDate"
-      :max-date="maxDate" title="计划"
-      :poppable="false"
-      :show-confirm="false"
-      @select="onCalendarSelect"
-    />
-  </van-list>
+    :formatter="formatter"
+    title="计划"
+    :poppable="false"
+    :show-confirm="false"
+    @select="onCalendarSelect"
+  />
 </template>
 
-<style scoped>
+<style scoped lang="less">
 
 </style>
 
