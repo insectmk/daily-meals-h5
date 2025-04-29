ea5404901548fac39642f66bcc7b64cabcb2aa22
InsectMk <makun_ing@126.com>
2025-04-29 14:49:28 +0800

    chore: 菜谱页面增加公共tab
    菜谱页面的tab单独抽离为组件，方便维护

--- src/pages/recipe-cache/index.vue
+++ src/pages/recipe-cache/index.vue
@@ -1,70 +1,14 @@
 <script setup lang="ts">
-import { page as getRecipesPage, publicRecipePage } from '@/api/recipe'
-import type { RecipeInfo } from '@/api/recipe/type'
-import RecipeCardList from '@/pages/recipe-cache/component/recipe-card-list.vue'
 import RecipeAddBubble from '@/pages/recipe-cache/component/recipe-add-bubble.vue'
+import RecipeSelf from '@/pages/recipe-cache/tab-pages/RecipeSelf.vue'
+import RecipePub from '@/pages/recipe-cache/tab-pages/RecipePub.vue'
+import RecipeSystem from '@/pages/recipe-cache/tab-pages/RecipeSystem.vue'
 
 defineOptions({
   name: 'RecipeCache',
 })
 
-let pageNo = 0 // 当前页
-let pubPageNo = 0 // 公共菜谱当前页
-const pageSize = 10 // 每页行数
-const recipeList = ref<RecipeInfo[]>([]) // 菜谱列表
-const loading = ref(false) // 加载
-const finished = ref(false) // 结束
-const pubRecipeList = ref<RecipeInfo[]>([]) // 公共菜谱列表
-const pubLoading = ref(false) // 公共菜谱-加载
-const pubFinished = ref(false) // 公共菜谱-结束
-const recipeTabActive = ref('self') // 当前所在tab
-// const { t } = useI18n() // 多语言支持
-
-/**
- * 加载
- */
-function onLoad() {
-  pageNo++ // 页面加1
-  // 获取菜单
-  getRecipesPage({
-    pageNo,
-    pageSize,
-  }).then((res) => {
-    // 将菜谱添加到列表中
-    res.data.list.forEach((recipe) => {
-      recipeList.value.push(recipe)
-    })
-    // 关闭加载
-    loading.value = false
-    if (res.data.list.length < pageSize) {
-      // 所有数据刷新完成
-      finished.value = true
-    }
-  })
-}
-
-/**
- * 加载公共菜谱
- */
-function pubOnLoad() {
-  pubPageNo++ // 页面加1
-  // 获取公共菜谱
-  publicRecipePage({
-    pageNo: pubPageNo,
-    pageSize,
-  }).then((res) => {
-    // 将菜谱添加到列表中
-    res.data.list.forEach((recipe) => {
-      pubRecipeList.value.push(recipe)
-    })
-    // 关闭加载
-    pubLoading.value = false
-    if (res.data.list.length < pageSize) {
-      // 所有数据刷新完成
-      pubFinished.value = true
-    }
-  })
-}
+const recipeTabActive = ref('system') // 当前所在tab
 
 // 用于记录滚动状态
 const scrollTop = ref(0)
@@ -85,29 +29,26 @@ onBeforeRouteLeave(() => {
   <RecipeAddBubble />
   <van-tabs
     v-model:active="recipeTabActive"
-    type="card" swipeable sticky
+    animated swipeable sticky
+    background="transparent"
   >
-    <van-tab name="self">
+    <van-tab name="system">
       <template #title>
-        <van-icon name="more-o" />我的菜谱
+        系统菜谱
       </template>
-      <RecipeCardList
-        v-model:loading="loading"
-        :finished="finished"
-        :recipe-list="recipeList"
-        @load="onLoad"
-      />
+      <RecipeSystem />
     </van-tab>
     <van-tab name="public">
       <template #title>
-        <van-icon name="more-o" />公共菜谱
+        公共菜谱
       </template>
-      <RecipeCardList
-        v-model:loading="pubLoading"
-        :finished="pubFinished"
-        :recipe-list="pubRecipeList"
-        @load="pubOnLoad"
-      />
+      <RecipePub />
+    </van-tab>
+    <van-tab name="self">
+      <template #title>
+        我的菜谱
+      </template>
+      <RecipeSelf />
     </van-tab>
   </van-tabs>
 </template>
---
+++ src/pages/recipe-cache/tab-pages/RecipeSelf.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import type { RecipeInfo } from '@/api/recipe/type'
+import RecipeCardList from '@/pages/recipe-cache/component/recipe-card-list.vue'
+import { page as getRecipesPage } from '@/api/recipe'
+
+let pageNo = 0 // 当前页
+const pageSize = 10 // 每页行数
+const recipeList = ref<RecipeInfo[]>([]) // 菜谱列表
+const loading = ref(false) // 加载
+const finished = ref(false) // 结束
+
+/**
+ * 加载
+ */
+function onLoad() {
+  pageNo++ // 页面加1
+  // 获取菜单
+  getRecipesPage({
+    pageNo,
+    pageSize,
+  }).then((res) => {
+    // 将菜谱添加到列表中
+    res.data.list.forEach((recipe) => {
+      recipeList.value.push(recipe)
+    })
+    // 关闭加载
+    loading.value = false
+    if (res.data.list.length < pageSize) {
+      // 所有数据刷新完成
+      finished.value = true
+    }
+  })
+}
+</script>
+
+<template>
+  <RecipeCardList
+    v-model:loading="loading"
+    style="min-height: 100vh;"
+    :finished="finished"
+    :recipe-list="recipeList"
+    @load="onLoad"
+  />
+</template>
+
+<style scoped>
+
+</style>
