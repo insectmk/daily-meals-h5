4bf02fb86ac1faaa9e7ed5e3f83c60c30ad03ac8
InsectMk <makun_ing@126.com>
2025-04-29 16:33:58 +0800

    chore: 优化菜谱列表组件，简化组件使用方式

--- src/pages/recipe-cache/component/recipe-card-list.vue
+++ src/pages/recipe-cache/component/recipe-card-list.vue
@@ -1,49 +1,51 @@
 <script setup lang="ts">
 import type { RecipeInfo } from '@/api/recipe/type'
 import RecipeCard from '@/pages/recipe-cache/component/recipe-card.vue'
-import type { CommonResult, PageResult } from '@/api/type'
+import type { CommonResult, PageParam, PageResult } from '@/api/type'
+import type { PropType } from 'vue'
 
-defineProps({
-  // 菜谱列表
-  recipeList: {
-    type: Array as () => RecipeInfo[],
+const props = defineProps({
+  // 获取菜谱的接口
+  recipeListApi: {
+    type: Function as PropType<(pageParam: PageParam) => Promise<CommonResult<PageResult<RecipeInfo>>>>,
     required: true,
-  },
-  // 加载
-  loading: {
-    type: Boolean,
-    required: false,
-    default: false,
-  },
-  // 是否结束
-  finished: {
-    type: Boolean,
-    required: false,
-    default: true,
   },
 })
 
-const emit = defineEmits([
-  // 加载
-  'load',
-  'update:loading',
-  'update:finished',
-])
-
-// 监听输入变化，触发双向绑定
-function handleLoading(value: string) {
-  emit('update:loading', value)
-}
+let pageNo = 0 // 当前页
+const pageSize = 10 // 每页行数
+const recipeList = ref<RecipeInfo[]>([]) // 菜谱列表
+const loading = ref(false) // 加载
+const finished = ref(false) // 结束
 
 const refreshing = ref<boolean>(false) // 下拉刷新
 
 /**
  * 加载更多
  */
-async function onLoad() {
-  emit('load', (_res: CommonResult<PageResult<RecipeInfo>>) => {
+
+/**
+ * 加载
+ */
+function onLoad() {
+  pageNo++ // 页面加1
+  // 获取菜单
+  props.recipeListApi({
+    pageNo,
+    pageSize,
+  }).then((res) => {
     // 刷新加载状态
     refreshing.value = false
+    // 将菜谱添加到列表中
+    res.data.list.forEach((recipe) => {
+      recipeList.value.push(recipe)
+    })
+    // 关闭加载
+    loading.value = false
+    if (res.data.list.length < pageSize) {
+      // 所有数据刷新完成
+      finished.value = true
+    }
   })
 }
 
@@ -52,24 +54,23 @@ async function onLoad() {
  */
 function onRefresh() {
   // 清空列表数据
-  emit('update:finished', false)
+  finished.value = false
   // 重新加载数据
   onLoad()
   // 将 loading 设置为 true，表示处于加载状态
-  emit('update:loading', true)
+  loading.value = true
 }
 </script>
 
 <template>
   <van-pull-refresh v-model="refreshing" @refresh="onRefresh">
     <van-list
+      v-model:loading="loading"
       style="min-height: 80vh;"
-      :loading="loading"
       :finished="finished"
       finished-text="已经到底啦 ~"
       loading-text="加载中..."
       @load="onLoad"
-      @update:loading="handleLoading"
     >
       <van-row>
         <van-col
--- src/pages/recipe-cache/tab-pages/RecipeSystem.vue
+++ src/pages/recipe-cache/tab-pages/RecipeSystem.vue
@@ -1,46 +1,11 @@
 <script setup lang="ts">
-import type { RecipeInfo } from '@/api/recipe/type'
 import RecipeCardList from '@/pages/recipe-cache/component/recipe-card-list.vue'
 import { systemRecipePage } from '@/api/recipe'
-import type { CommonResult, PageResult } from '@/api/type'
-
-let pageNo = 0 // 当前页
-const pageSize = 10 // 每页行数
-const recipeList = ref<RecipeInfo[]>([]) // 菜谱列表
-const loading = ref(false) // 加载
-const finished = ref(false) // 结束
-
-/**
- * 加载
- */
-function onLoad(callback: (res: CommonResult<PageResult<RecipeInfo>>) => void) {
-  pageNo++ // 页面加1
-  // 获取菜单
-  systemRecipePage({
-    pageNo,
-    pageSize,
-  }).then((res) => {
-    callback(res) // 回调
-    // 将菜谱添加到列表中
-    res.data.list.forEach((recipe) => {
-      recipeList.value.push(recipe)
-    })
-    // 关闭加载
-    loading.value = false
-    if (res.data.list.length < pageSize) {
-      // 所有数据刷新完成
-      finished.value = true
-    }
-  })
-}
 </script>
 
 <template>
   <RecipeCardList
-    v-model:loading="loading"
-    :finished="finished"
-    :recipe-list="recipeList"
-    @load="onLoad"
+    :recipe-list-api="systemRecipePage"
   />
 </template>
 
