e5588a21c8c558a925c2f59684be3f8c999a20f9
InsectMk <makun_ing@126.com>
2025-05-08 16:58:24 +0800

    chore: 增加菜谱的【加入菜单】功能

--- src/pages/recipe/action-func-bar/func-icon/add-to-menu.vue
+++ src/pages/recipe/action-func-bar/func-icon/add-to-menu.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import { DICT_TYPE } from '@/constants/dict'
-import type { CreatePlanReq } from '@/api/dailyplan/type'
-import { addRecipesPlan } from '@/api/dailyplan'
 import ResponseCode from '@/constants/response-code'
-import useRouteCacheStore from '@/stores/modules/routeCache'
+import { getSelfRecipeMenuList } from '@/api/recipe-menu'
+import type { MenuRecipe } from '@/api/menu-recipe/type'
+import { addMenuRecipe } from '@/api/menu-recipe'
 
 const props = defineProps({
   // 菜谱ID
@@ -15,29 +14,20 @@ const props = defineProps({
 
 const addToPlanDialogShow = ref<boolean>(false) // 加入到日期计划弹窗
 
-const dailyPlanForm = reactive<CreatePlanReq>({
-  recipeIds: [], // 菜谱ID
-  planDate: Date.now(), // 计划日期
-  mealType: 0, // 计划类型
+const menuRecipeForm = reactive<MenuRecipe>({
+  recipeMenuId: props.recipeId, // 菜谱ID
+  recipeId: null, // 菜谱ID
   memo: '', // 备注
 })
 const isToday = ref<boolean>(false) // 是否今日
 
 /**
- * 加入到菜谱计划
+ * 加入到菜谱菜单
  */
-function addToPlan() {
-  dailyPlanForm.recipeIds = [props.recipeId] // 菜谱ID
-  if (isToday.value) {
-    dailyPlanForm.planDate = Date.now() // 为今日，计划日期为今日
-  }
-  addRecipesPlan(dailyPlanForm).then((res) => {
+function addToMenu() {
+  addMenuRecipe(menuRecipeForm).then((res) => {
     if (res.code === ResponseCode.SUCCESS.code) {
-      showNotify({ type: 'success', message: `成功加入到计划！`, duration: 1500 })
-      // 清除菜谱计划的缓存
-      const routeCacheStore = useRouteCacheStore()
-      routeCacheStore.removeCache('PlanCache') // 菜谱计划
-      routeCacheStore.removeCache('PlanInfo') // 计划详细信息
+      showNotify({ type: 'success', message: `成功加入到菜单！`, duration: 1500 })
     }
   })
   return false
@@ -53,25 +43,24 @@ function addToPlan() {
     }"
   />
 
-  <!-- 加入指定日期 -->
+  <!-- 加入指定菜单 -->
   <van-dialog
     v-model:show="addToPlanDialogShow"
     title="加入计划"
     show-cancel-button
-    @confirm="addToPlan"
+    @confirm="addToMenu"
   >
-    <mk-form-calendar
-      v-model="dailyPlanForm.planDate"
-      label="计划日期"
-      placeholder="点击选择日期"
-    />
     <mk-form-picker
-      v-model="dailyPlanForm.mealType"
-      :dict-type="DICT_TYPE.MEALS_MEAL_TYPE"
-      label="计划类型" placeholder="点击选择类型"
+      v-model="menuRecipeForm.recipeId"
+      :dict-type="getSelfRecipeMenuList"
+      label="菜单" placeholder="点击选择菜单"
+      :custom-field-name="{
+        text: 'title',
+        value: 'id',
+      }"
     />
     <mk-form-text
-      v-model="dailyPlanForm.memo"
+      v-model="menuRecipeForm.memo"
       :maxlength="255"
       label="备注"
       placeholder="请输入备注"
