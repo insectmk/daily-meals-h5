001ae43b5f8fd6a0a755874e4ea2d773cbcf054d
InsectMk <makun_ing@126.com>
2025-05-08 14:30:08 +0800

    chore: 填充菜谱菜单对象字段

--- src/api/recipe-menu/type.ts
+++ src/api/recipe-menu/type.ts
@@ -2,5 +2,18 @@
  * 菜谱菜单
  */
 export interface RecipeMenu {
-
+  // 编号
+  id: number
+  // 标题
+  title: string
+  // 副标题
+  subtitle: string
+  // 菜单描述
+  menuDesc: string
+  // 菜单封面图
+  picUrl: string
+  // 备注
+  memo: string
+  // 创建时间
+  createTime: number
 }
