a97ad50e9ce63d4ba07b630c4d4f8ddcd1ecfa4f
InsectMk <makun_ing@126.com>
2025-05-21 15:23:52 +0800

    feat: 新增菜谱菜单的修改功能

--- src/pages/profile/self-content-tab/recipe-menu-tab.vue
+++ src/pages/profile/self-content-tab/recipe-menu-tab.vue
@@ -1,77 +1,21 @@
 <script setup lang="ts">
-import { createRecipeMenu, getSelfRecipeMenuPage } from '@/api/recipe-menu'
-import type { RecipeMenu } from '@/api/recipe-menu/type'
-import ResponseCode from '@/constants/response-code'
-import { DICT_TYPE } from '@/constants/dict'
+import { getSelfRecipeMenuPage } from '@/api/recipe-menu'
+import { useRouter } from 'vue-router'
 
-const addRecipeMenuDialogShow = ref<boolean>(false) // 添加菜谱菜单弹窗
-
-const recipeMenuForm = reactive<RecipeMenu>({
-  title: '', // 标题
-  subtitle: '', // 副标题
-  menuDesc: '', // 菜单描述
-  menuStatus: null, // 菜单状态
-  picUrl: '', // 菜单封面图
-  memo: '', // 备注
-})
-
-/**
- * 添加菜谱菜单
- */
-function addRecipeMenu() {
-  createRecipeMenu(recipeMenuForm).then((res) => {
-    if (res.code === ResponseCode.SUCCESS.code) {
-      showNotify({ type: 'success', message: `菜谱菜单添加成功！`, duration: 1500 })
-    }
-  })
-}
+const router = useRouter()
 </script>
 
 <template>
-  <van-cell title="新建菜单" is-link @click="addRecipeMenuDialogShow = true" />
+  <van-cell
+    title="新建菜单" is-link @click="() => {
+      // 打开录入页面
+      router.push({
+        name: 'RecipeMenuForm', // 需指定路由名称
+        //query: { recipeId: props.recipe.id },
+      })
+    }"
+  />
   <RecipeMenuCardList :menu-list-api="getSelfRecipeMenuPage" min-height="55vh" />
-  <!-- 新建菜单弹窗 -->
-  <van-dialog
-    v-model:show="addRecipeMenuDialogShow"
-    title="新建菜单"
-    teleport="body"
-    show-cancel-button
-    @confirm="addRecipeMenu"
-  >
-    <van-field
-      v-model="recipeMenuForm.title" label="标题"
-      :rules="[{ required: true, message: '请填写菜单标题' }]"
-      placeholder="请输入菜单标题"
-    />
-    <van-field
-      v-model="recipeMenuForm.subtitle" label="副标题"
-      :rules="[{ required: true, message: '请填写菜单副标题' }]"
-      placeholder="请输入菜单副标题"
-    />
-    <mk-form-tiptop
-      v-model="recipeMenuForm.menuDesc"
-      :rules="[{ required: true, message: '请填写菜单描述' }]"
-      label="步骤"
-      :maxlength="2000"
-    />
-    <mk-form-img
-      v-model="recipeMenuForm.picUrl"
-      :rules="[{ required: true, message: '请上传封面' }]"
-      label="封面"
-    />
-    <mk-form-picker
-      v-model="recipeMenuForm.menuStatus"
-      :rules="[{ required: true, message: '请选择菜单状态' }]"
-      :dict-type="DICT_TYPE.MEALS_STATIC_RECIPE_STATUS"
-      label="菜单状态" placeholder="点击选择状态"
-    />
-    <mk-form-text
-      v-model="recipeMenuForm.memo"
-      :maxlength="255"
-      label="备注"
-      placeholder="请输入备注"
-    />
-  </van-dialog>
 </template>
 
 <style scoped>
--- src/types/typed-router.d.ts
+++ src/types/typed-router.d.ts
@@ -53,6 +53,7 @@ declare module 'vue-router/auto-routes' {
     'RecipeListCategory': RouteRecordInfo<'RecipeListCategory', '/recipe/tab-pages/recipe-category/:categoryId', { categoryId: ParamValue<true> }, { categoryId: ParamValue<false> }>,
     '/recipe/tab-pages/RecipeSelf': RouteRecordInfo<'/recipe/tab-pages/RecipeSelf', '/recipe/tab-pages/RecipeSelf', Record<never, never>, Record<never, never>>,
     'RecipeMenuInfo': RouteRecordInfo<'RecipeMenuInfo', '/recipe-menu/:id', { id: ParamValue<true> }, { id: ParamValue<false> }>,
+    'RecipeMenuForm': RouteRecordInfo<'RecipeMenuForm', '/recipe-menu/form', Record<never, never>, Record<never, never>>,
     '/recipe-menu/more-popup/': RouteRecordInfo<'/recipe-menu/more-popup/', '/recipe-menu/more-popup', Record<never, never>, Record<never, never>>,
     'register': RouteRecordInfo<'register', '/register', Record<never, never>, Record<never, never>>,
     'ScrollCache': RouteRecordInfo<'ScrollCache', '/scroll-cache', Record<never, never>, Record<never, never>>,
