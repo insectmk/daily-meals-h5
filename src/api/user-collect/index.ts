084fd51eee67e5a4cf5819892321a664ba822611
InsectMk <makun_ing@126.com>
2025-05-22 14:27:03 +0800

    chore: 增加页查询自己的收藏夹信息接口

--- src/api/user-collect/index.ts
+++ src/api/user-collect/index.ts
@@ -1,5 +1,6 @@
 import type { UserCollect } from '@/api/user-collect/type'
 import request from '@/utils/request'
+import type { PageParam, PageResult } from '@/api/type'
 
 /**
  * 创建或更新用户收藏夹，通过id判断
@@ -8,4 +9,12 @@ import request from '@/utils/request'
  */
 export function createUserCollect(param: UserCollect) {
   return request.post<number>('/meals/user-collect/create-or-update', param)
+}
+
+/**
+ * 分页查询自己的收藏夹信息
+ * @param pageReqVO
+ */
+export function getSelfUserCollectPage(pageReqVO: PageParam) {
+  return request.get<PageResult<UserCollect>>('/meals/user-collect/page-self', pageReqVO)
 }
