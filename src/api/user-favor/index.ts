40a03f44d40e87701165451da5cf20d497d9358d
InsectMk <makun_ing@126.com>
2025-05-23 10:15:45 +0800

    chore: 调整收藏接口

--- src/api/user-favor/index.ts
+++ src/api/user-favor/index.ts
@@ -1,18 +1,18 @@
-import type { UserFavor } from '@/api/user-favor/type'
+import type { UserFavorCancelParam, UserFavorCreatParam } from '@/api/user-favor/type'
 import request from '@/utils/request'
 
 /**
  * 添加用户收藏
  * @param param
  */
-export function addUserFavor(param: UserFavor) {
-  return request.post<number>('/meals/user-favor/create', param)
+export function addUserFavor(param: UserFavorCreatParam) {
+  return request.post<boolean>('/meals/user-favor/create', param)
 }
 
 /**
- * 删除用户收藏
- * @param id 收藏记录ID
+ * 取消用户收藏
+ * @param params 取消参数
  */
-export function deleteUserFavor(id: number) {
-  return request.del<boolean>('/meals/user-favor/delete', { id })
+export function cancelUserFavor(params: UserFavorCancelParam) {
+  return request.del<boolean>('/meals/user-favor/cancel', params)
 }
