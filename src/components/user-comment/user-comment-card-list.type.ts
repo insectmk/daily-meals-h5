d8891181cc3d985dc3068a95fe36af56d5c4c4a0
InsectMk <makun_ing@126.com>
2025-05-29 13:21:14 +0800

    chore: 增加用户评论卡片列表组件

---
+++ src/components/user-comment/user-comment-card-list.type.ts
@@ -0,0 +1,6 @@
+/**
+ * 导出的内容
+ */
+export interface UserCommentCardListExposed {
+  query: () => void // 列表查询
+}
---
+++ src/components/user-comment/user-comment-card.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import type { UserComment } from '@/api/user-comment/type'
+
+defineProps({
+  comment: {
+    type: Object as PropType<UserComment>,
+    required: true,
+  },
+})
+
+const router = useRouter()
+</script>
+
+<template>
+  <van-card
+    :title="comment.commentContent"
+    :desc="comment.commentContent"
+    tag="菜单"
+    :thumb="comment.userAvatar"
+    @click="router.push(`/recipe-menu/${comment.id}`)"
+  />
+</template>
+
+<style scoped>
+
+</style>
