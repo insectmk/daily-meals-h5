11ef53ac62f0693bea07ef4a4bfd60c4207233bf
InsectMk <makun_ing@126.com>
2025-05-30 17:36:42 +0800

    chore: 我的消息 增加评论的列表的展示

--- src/pages/message/comment/index.vue
+++ src/pages/message/comment/index.vue
@@ -1,11 +1,33 @@
 <script setup lang="ts">
+import router from '@/router'
+import { getCommentPage } from '@/api/user-comment'
+
 defineOptions({
   name: 'MessageComment',
 })
+
+/**
+ * 取消操作
+ */
+function onCancel() {
+  if (window.history.state.back)
+    history.back()
+  else
+    router.replace('/')
+}
 </script>
 
 <template>
-  <h1>我的评论</h1>
+  <van-nav-bar
+    :title="$t('message.comment.title')"
+    :left-text="$t('common.back')"
+    left-arrow placeholder fixed
+    @click-left="onCancel"
+  />
+  <user-comment-card-list
+    :list-api="getCommentPage"
+    min-height="80vh"
+  />
 </template>
 
 <style scoped>
@@ -17,7 +39,8 @@ defineOptions({
   name: 'MessageComment',
   meta: {
     title: '评论',
-    i18n: 'message.title'
+    i18n: 'message.title',
+    customNav: true,
   },
 }
 </route>
--- src/pages/message/reply/index.vue
+++ src/pages/message/reply/index.vue
@@ -1,11 +1,33 @@
 <script setup lang="ts">
+import router from '@/router'
+import { getCommentPage } from '@/api/user-comment'
+
 defineOptions({
   name: 'MessageReply',
 })
+
+/**
+ * 取消操作
+ */
+function onCancel() {
+  if (window.history.state.back)
+    history.back()
+  else
+    router.replace('/')
+}
 </script>
 
 <template>
-  <h1>回复我的</h1>
+  <van-nav-bar
+    :title="$t('message.reply.title')"
+    :left-text="$t('common.back')"
+    left-arrow placeholder fixed
+    @click-left="onCancel"
+  />
+  <user-comment-card-list
+    :list-api="getCommentPage"
+    min-height="80vh"
+  />
 </template>
 
 <style scoped>
@@ -14,10 +36,11 @@ defineOptions({
 
 <route lang="json5">
 {
-  name: 'MessageReply',
-  meta: {
-    title: '回复',
-    i18n: 'message.title'
-  },
+name: 'MessageReply',
+meta: {
+  title: '回复我的',
+  i18n: 'message.reply.title',
+  customNav: true,
+},
 }
 </route>
