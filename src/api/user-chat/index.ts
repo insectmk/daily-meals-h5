0f2a5f9532bb4609102bf3ce6ab1f66e81c1103d
InsectMk <makun_ing@126.com>
2025-06-13 11:29:09 +0800

    chore: 增加获取用户历史消息的接口

--- src/api/user-chat/index.ts
+++ src/api/user-chat/index.ts
@@ -1,5 +1,5 @@
 import request from '@/utils/request'
-import type { ChatMessageSendReq } from '@/api/user-chat/type'
+import type { ChatMessageInfo, ChatMessageSendReq, UserChatMessagePageParam } from '@/api/user-chat/type'
 
 /**
  * 发送消息
@@ -7,4 +7,12 @@ import type { ChatMessageSendReq } from '@/api/user-chat/type'
  */
 export function sendUserChatMessage(param: ChatMessageSendReq) {
   return request.post<number>('/meals/user-chat-message/send', param)
+}
+
+/**
+ * 获得用户聊天消息列表
+ * @param pageParam 分页条件
+ */
+export function getUserChatMessageList(pageParam: UserChatMessagePageParam) {
+  return request.get<ChatMessageInfo[]>('/meals/user-chat-message/list', pageParam)
 }
--- src/api/user-chat/type.ts
+++ src/api/user-chat/type.ts
@@ -6,3 +6,27 @@ export interface ChatMessageSendReq {
   contentType: number // 消息类型
   content: string // 消息
 }
+/**
+ * 用户聊天消息
+ */
+export interface ChatMessageInfo {
+  id: number // 编号
+  conversationId: number // 会话编号
+  senderUserId: number // 发送人编号
+  senderUserAvatar: string // 发送人头像
+  receiverUserId: number // 接收人编号
+  contentType: number // 消息类型
+  content: string // 消息
+  readStatus: boolean // 是否已读
+  createTime: number // 创建时间
+}
+
+/**
+ * 用户聊天消息分页请求
+ */
+export interface UserChatMessagePageParam {
+  conversationId: number // 会话编号
+  createTime: number // 发送时间，小于该发送时间
+  receiverUserId: number// 接收方用户编码
+  limit: number// 每次查询条数，最大值为 100
+}
